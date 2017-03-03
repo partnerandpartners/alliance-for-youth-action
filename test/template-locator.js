4174f16f0f96599f8541009ad9d92dd12f43f09e
Stephen Winsor <stephen.g.winsor@gmail.com>
2017-03-03 10:54:12 -0500

    Adding support for 404, search, post_type_archive post types.

--- test/template-locator.js
+++ test/template-locator.js
@@ -3,20 +3,32 @@ var expect = require('chai').expect;
 
 var expectedTemplates = [
   {
-    input: {postType: 'post', id: 0, slug: 'test-post'},
+    input: {type: 'post', id: 0, slug: 'test-post'},
     expected: ['index', 'singular', 'single', 'single-post']
   },
   {
-    input: {postType: 'page', id: 10, slug: 'this-is-page-slug'},
+    input: {type: 'page', id: 10, slug: 'this-is-page-slug'},
     expected: ['index', 'singular', 'page', 'page-10', 'page-this-is-page-slug']
+  },
+  {
+    input: {type: 'search'},
+    expected: ['index', 'search']
+  },
+  {
+    input: {type: '404'},
+    expected: ['index', '404']
+  },
+  {
+    input: {type: 'post_type_archive', post_type_archive_type: 'custom-post-type'},
+    expected: ['index', 'archive', 'archive-custom-post-type']
   }
 ];
 
 describe('TemplateLocator', function(){
-  describe('Expectations', function(){
-    expectedTemplates.forEach(function(element){
-      it('Should match expected results', function(){
-        var result = templateLocator(element.input.postType, element.input.id, element.input.slug)
+  describe('Locates templates for various page types', function(){
+    expectedTemplates.forEach(function(element, index){
+      it('Should match expected results for test input type: ' + element.input.type, function(){
+        var result = templateLocator(element.input)
 
         expect(result).to.deep.equal(element.expected)
       })
